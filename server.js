e66c44613cfdb85830cbce787583a80c2d44f64e
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2020-10-15 18:33:03 +0530

    sm fix

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const {Router} =require("express");
 
-const port = process.env.PORT || 8000;
+const port = 8000;
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({extended: false}));
